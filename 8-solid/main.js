12f5aa0f96c587ab7346785081a916507e62c460
Alexander <EVDA17@mail.ru>
2025-01-05 02:32:44 +0300

    я поменял, но не очень понял

--- 8-solid/main.js
+++ 8-solid/main.js
@@ -1,38 +1,44 @@
 class Billing {
-  constructor(amount) {
-    this.amount = amount;
-  }
+  #amount = 100;
   calculateTotal() {
-    return this.amount;
+    return this.#amount;
   }
 }
 class fixBilling extends Billing {
+  #amount;
   constructor(amount) {
-    super(amount);
+    super();
+    this.#amount = amount;
   }
 }
 
 class hourBilling extends Billing {
+  #amount;
+  #hour;
   constructor(amount, hour = 0) {
-    super(amount);
-    this.hour = hour;
+    super();
+    this.#amount = amount;
+    this.#hour = hour;
   }
   calculateTotal() {
-    return this.amount * this.hour;
+    return this.#amount * this.#hour;
   }
 }
 
 class itemBilling extends Billing {
+  #amount;
+  #elementsAmount;
   constructor(amount, elementsAmount = 0) {
-    super(amount);
-    this.elementsAmount = elementsAmount;
+    super();
+    this.#amount = amount;
+    this.#elementsAmount = elementsAmount;
   }
   calculateTotal() {
-    return this.amount * this.elementsAmount;
+    return this.#amount * this.#elementsAmount;
   }
 }
 
-console.log(new Billing(3).calculateTotal())
-console.log(new fixBilling(3).calculateTotal())
-console.log(new hourBilling(10, 3).calculateTotal())
-console.log(new itemBilling(5, 14).calculateTotal())
+console.log(new Billing().calculateTotal());
+console.log(new fixBilling(3).calculateTotal());
+console.log(new hourBilling(10, 3).calculateTotal());
+console.log(new itemBilling(5, 14).calculateTotal());
